66a737d552769ed4153a6bd4c4d2035dd4fc97d8
5ndoi <motch.hawk.110@gmail.com>
2024-12-14 12:31:49 +0900

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,18 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+# マウス座標
+mouse_x, mouse_y = -1, -1
+
+# マウスのコールバック関数
+def mouse_callback(event, x, y, flags, param):
+    global mouse_x, mouse_y
+    if event == cv2.EVENT_MOUSEMOVE:
+        cv2.circle(img, (x, y), 1, (0, 255, 255), -1)
+
 img = np.full((800,800,3), 0)
 
-st.image(img)
+while True:
+    cv2.setMouseCallback("FaceMesh", mouse_callback)
+    st.image(img)
+
