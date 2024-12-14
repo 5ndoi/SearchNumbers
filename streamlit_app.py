c69fb0e3516131621f9d039c5f69f7e3f2679630
5ndoi <motch.hawk.110@gmail.com>
2024-12-14 12:09:58 +0900

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import cv2
+import numpy as np
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+image = np.full((800,800,3), 255)
