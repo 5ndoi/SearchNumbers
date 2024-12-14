72ca31ca4df924f0d3637886f9a5e088883e6adb
5ndoi <motch.hawk.110@gmail.com>
2024-12-14 12:35:05 +0900

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,12 @@ mouse_x, mouse_y = -1, -1
 def mouse_callback(event, x, y, flags, param):
     global mouse_x, mouse_y
     if event == cv2.EVENT_MOUSEMOVE:
-        cv2.circle(img, (x, y), 1, (0, 255, 255), -1)
+        cv2.circle(img, (x, y), 1, (255, 255, 255), -1)
 
-img = np.full((800,800,3), 0)
 window = st.empty()
 
 while True:
+    img = np.full((800,800,3), 0)
     cv2.setMouseCallback("FaceMesh", mouse_callback)
     window.image(img)
 
